6e13b88b58f1911476b9f0eb5451e64a69262a72
Reena079 <frfr0227@gmail.com>
2025-04-10 20:55:25 +0400

    Primitive Quiz
    Primitive Quiz

---
+++ Exercise4.py
@@ -0,0 +1,25 @@
+#Function to ask a question
+def ask_question(question,correct_answer):
+    answer = input(question).strip().lower() #convert answer to lowercase
+    if answer==correct_answer.lower(): #ignore capitalization
+        print("Correct!")
+    else:
+        print("Wrong")
+
+# List of European countries and capitals
+questions= [
+    ("What is the capital of France?","Paris"),
+    ("What is the capital of United Kingdom?","London"),
+    ("What is the capital of Belgium?","Brussels"),
+    ("What is the capital of Greece?","Athens"),
+    ("What is the capital of Spain?","Madrid"),
+    ("What is the capital of Italy?","Rome"),
+    ("What is the capital of Hungary?","Budapest"),
+    ("What is the capital of Germany?","Berlin"),
+    ("What is the capital of Sweden?","Stockholm"),
+    ("What is the capital of Netherlands?","Amsterdam"),
+]
+
+# Check each question and ask for answer
+for question, correct_answer in questions:
+    ask_question(question, correct_answer)
