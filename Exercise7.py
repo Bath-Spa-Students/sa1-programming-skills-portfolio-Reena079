0731ae941859770f08c830498c5d37acd1a71596
Reena079 <frfr0227@gmail.com>
2025-04-10 21:36:47 +0400

    Create Exercise7.py
    Some Counting

---
+++ Exercise7.py
@@ -0,0 +1,29 @@
+# Counting from 0 to 50 upwards
+print("Counting up from 0 to 50 in increments of 1:")
+for i in range (0,51,1):
+    print(i,end=" ")
+print("\n")
+
+#Counting from 0 to 50 downwards
+print("Counting down from 50 to 0 in decrements of 1:")
+for i in range(50,-1,-1):
+   print(i,end=" ")
+print("\n")
+
+#Counting up from 30 to 50 upwards
+print("Counting up from 30 to 50 in increments of 1:")
+for i in range(30,51,1):
+    print(i,end=" ")
+print("\n")
+
+# counting downwards from 50 to 10 by skipping 2 numbers
+print("Counting down from 50 to 10 in decrements of 2:")
+for i in range(50,9,-2):
+     print(i,end=" ")
+print("\n")
+
+#counting from 100 to 200, but we are adding 5 each time counts
+print("Counting up from 100 to 200 in increments of 5")
+for i in range(100,201,5):
+    print(i,end=" ")
+print("\n")
